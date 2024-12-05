f62d04ab03d94ded23cf63df379b643d0e2f88fa
song-chaeyoung <153435389+song-chaeyoung@users.noreply.github.com>
2024-12-05 15:29:27 +0900

    Update db.json

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
 export const myData = () => {
   try {
     return fetch(
-      // "https://raw.githubusercontent.com/song-chaeyoung/portfolioPage/refs/heads/main/db.json"
-      "../db.json"
+      "https://raw.githubusercontent.com/song-chaeyoung/portfolioPage/refs/heads/main/db.json"
+      // "../db.json"
     ).then((res) => res.json());
   } catch (err) {
     console.log(err);
